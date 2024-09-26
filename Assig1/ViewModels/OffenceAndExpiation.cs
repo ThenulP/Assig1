5067ada8b732dad69c037e8e51ddf3180213aac1
ThenulP <147567427+ThenulP@users.noreply.github.com>
2024-09-26 19:38:44 +0930

    Create OffenceAndExpiation.cs
    Creating custom ViewModel for Details View

---
+++ Assig1/ViewModels/OffenceAndExpiation.cs
@@ -0,0 +1,31 @@
+﻿using Assig1.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assig1.ViewModels
+{
+    public class OffenceAndExpiation
+    {
+        [Display(Name = "Incident Date")]
+        public DateOnly IncidentStartDate { get; set; }
+
+        [Display(Name = "Vehicle Speed")]
+        public int? VehicleSpeed { get; set; }
+
+        [Display(Name = "Speed Limit")]
+        public int? LocationSpeedLimit { get; set; }
+
+        [Display(Name = "Driver State")]
+        public string? DriverState { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Display(Name = "Total Fee")]
+        public int? TotalFeeAmt { get; set; }
+
+        [Display(Name = "Offence")]
+        public virtual Offence? Offence { get; set; }
+
+        [Display(Name = "Section")]
+        public virtual Section? Section { get; set; }
+
+    }
+}
